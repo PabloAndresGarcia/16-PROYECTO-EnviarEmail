2c4dc308844030736ab92ce9dea315313c6a3f9e
Pablo Andres Garcia <garciasjs@gmail.com>
2022-11-29 09:28:00 -0300

    Spinner

--- js/app.js
+++ js/app.js
@@ -13,10 +13,35 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario')
     const btnSubmit = document.querySelector('#formulario button[type="submit"]')
+    const btnReset = document.querySelector('#formulario button[type="reset"]')
+    const spinner = document.querySelector('#spinner')
+
+
     //Asignar eventos
     inputEmail.addEventListener('input', validar);
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
+    formulario.addEventListener('submit', enviarEmail);
+
+
+    btnReset.addEventListener('click', function(e){
+        e.preventDefault();
+
+        email.email = '';
+        email.asunto = '';
+        email.mensaje = '';
+        formulario.reset();
+        comprobarEmail();
+    })
+    function enviarEmail(e){
+        e.preventDefault();
+        spinner.classList.add('flex');
+        
+        spinner.classList.remove('hidden');
+
+    }
+
+    
 
 
     function validar(e) {
